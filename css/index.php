c33c07ee901e59a62a5662fa139035f9f4951445
Carolina Noriega Rincon <112576449+cnoriegar26@users.noreply.github.com>
2022-09-19 10:25:08 -0500

    Add files via upload

---
+++ css/index.php
@@ -0,0 +1,239 @@
+<html >
+<head>
+<?php include("head.php"); ?>
+<link rel="stylesheet" href="../css/index.css">
+</head>
+<body>
+
+<section>
+	<?php include("navegador1.php");?>
+</section>
+<div class="col-md-12 barra2"></div>
+
+<section>
+	<?php include("barra.php"); ?>
+</section>
+
+<section>
+<div class="container contenedor">
+	<div class="contenedor2" align="center">
+		<h1>palacio hindu</h1>
+	</div>
+</div>
+</section>
+
+<section class="navegador_2">
+	<?php include("navegador.php");?>
+</section>
+
+<section>
+
+<div class="col-md-12" style="padding: 0">
+	<div class="row" style="margin:0;">
+		<div class="col-md-2"></div>
+		<div class="col-md-8" align="center">
+			<div class="row" >
+			<div class="col-md-12 titulo_1">
+				<img src="../img/iconos/img_nuestra_emp.png">¿NUESTRA EMPRESA?
+			</div>
+								
+			</div>		
+			<div class="row" style="padding-top: 40px;">
+				<div class="col-md-6" >
+					<img class="img_nuestra_emp" src="../img/iconos/planta.jpg">
+				</div>
+				<div class="col-md-6">
+				<p class="texto">
+					Ecopetrol cuenta desde hace varios años con un Código de Ética, construido de forma participativa, el cual se constituye en el referente formal e institucional de la conducta personal y profesional que deben tener todos los trabajadores.<br><br>
+
+					miembros de Junta Directiva, proveedores y contratistas de la Empresa, independiente del cargo o de la función que ocupen.
+				</p>
+				<p class="firma">palacio hindu</p>
+				
+				</div>
+			</div>
+		</div>
+		<div class="col-md-2" align="right" style="padding: 0"><img src="../img/iconos/jarron.jpg"></div>
+	</div>
+</div>
+</section>
+
+<section>
+<div class="col-md-12" style="padding: 0; ">
+	<div class="row" style="margin: 0">
+		<div class="col-md-2" style="padding: 0"><img src="../img/iconos/rama.jpg"></div>
+		<div class="col-md-8 botones" align="center">
+			<a type="buttom" class="btn" > <img src="../img/iconos/rama1.png" height="60px" width="60px"><br>MEDICINALES</a>
+			<a type="buttom" class="btn" > <img src="../img/iconos/rama2.png" height="60px" width="60px"><br>UNGÜENTO</a>
+			<a type="buttom" class="btn" > <img src="../img/iconos/rama3.png" height="60px" width="60px"><br>CONTRA</a>
+			<a type="buttom" class="btn" > <img src="../img/iconos/rama2.png" height="60px" width="60px"><br>CONTRA</a>
+		</div>
+		</div>
+		<div class="col-md-2"></div>
+	</div>
+</div>
+</section>
+
+<section>
+<div class="container matas" align="center" >
+	<div class="row" style="height: auto">
+		<div class="col-md-6" style="padding: 5px; height: 500px">
+			<div class="row" style="margin: 0; height: 100%">
+				<div class="col-md-12">
+					<div class="row">
+						<div class="col-md-6 " style="padding: 0">
+							<div class="col-md-12 cuadro_1 cuadros" >
+								<p>EL 25% DE LAS PERSONAS SUFRE DE DOLENCIA EN LAS ARTICULACIONES</p>
+							</div>
+							<div class="col-md-12 cuadro_3 cuadros" >
+								<img src="../img/iconos/mata2.jpg" height="100%" width="100%">
+							</div>
+						</div>
+						<div class="col-md-6 " style="padding: 0">
+							<div class="col-md-12 cuadro_2 cuadros"  align="center">
+								<img src="../img/iconos/mata1.jpg" height="100%" width="100%">
+							</div>
+							<div class="col-md-12 cuadro_4 cuadros">
+								<p>LA ESENCIA DE LIMON AYUDA A SENTIRTE MEJOR</p>	
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="col-md-6 margen_cuadro" style="padding: 5px; height: 500px; ">
+			<div class="row" style="margin: 0; height: 100%">
+				<div class="col-md-12">
+					<div class="row">
+						<div class="col-md-6 cuadro_5 cuadros" ><img src="../img/iconos/mata3.jpg" height="100%"; width="100%"></div>
+						<div class="col-md-6" style="padding: 0">
+							<div class="col-md-12  cuadro_6 cuadros" >
+								<p>EL CONSUMO DE SABILA ALIVIA EL ESTRÉS Y DOLENCIA EN LOS HUESOS</p>
+							</div>
+							<div class="col-md-12 cuadro_7 cuadros" ><img src="../img/iconos/mata4.jpg" height="100%" width="100%"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+</section>
+
+
+<section>
+<div class="row" style="margin: 0">
+<div class="container nuestros_prod" align="center">
+	<h1 class="titulo_3"><img src="../img/iconos/img_nuestra_emp.png">NUESTROS PRODUCTOS</h1>
+</div>
+</div>
+
+<div class="row" style="margin: 0">
+		<div class="col-md-12 fondo" >
+		<div class="container" style="height: 100%; padding: 0; ">
+			<div class="row" style="height: 100%; margin: 0; padding-top: 3% ">
+				
+				<div class="col-md-3" >
+				<div class="col-md-12" align="center">
+					<h1 class="h1_1">NUESTROS</h1>
+					<img class="img" src="../img/productos/producto1.jpg" height="100%" width="100%" >
+				</div></div>
+				<div class="col-md-3">
+					<h1 class="h1_2">NUESTROS</h1>
+					<img class="img2" src="../img/productos/producto2.jpg" >
+				</div>
+				<div class="col-md-3">
+					<h1 class="h1_1">NUESTROS</h1>
+					<img class="img" src="../img/productos/producto3.jpg" >
+				</div>
+				<div class="col-md-3">
+					<h1 class="h1_2">NUESTROS</h1>
+					<img class="img2" src="../img/productos/producto4.jpg" >
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+	
+</section>
+
+<div class="row" style="margin: 0; margin-top: 20px; margin-bottom: 20px">
+		<div class="container" align="center">
+			<h1 class="titulo_3"><img src="../img/iconos/img_nuestra_emp.png">OFERTAS</h1>
+		</div>
+	</div>
+	
+<div class="row" style="margin: 0">
+		<div class="col-md-12 oferta">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-7" >
+						<div class="row">
+							<div class="col-md-1 punto">
+								<img src="../img/iconos/punto verde.png"> 
+							</div>
+							<div class="col-md-10">
+								<h1>Ecopetrol cuenta </h1>
+								<p>
+								Desde hace varios años con un Código de Ética, construido de forma participativa, el cual se constituye en el referente formal.</p>
+
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-1 punto">
+								<img src="../img/iconos/punto claro.png"> 
+							</div>
+							<div class="col-md-10">
+								<h1>Ecopetrol cuenta </h1>
+								<p>
+								Desde hace varios años con un Código de Ética, construido de forma participativa, el cual se constituye en el referente formal.</p>
+
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-1 punto">
+								<img src="../img/iconos/punto azul.png"> 
+							</div>
+							<div class="col-md-10">
+								<h1>Ecopetrol cuenta </h1>
+								<p>
+								Desde hace varios años con un Código de Ética, construido de forma participativa, el cual se constituye en el referente formal.</p>
+
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-1 punto">
+								<img src="../img/iconos/punto magenta.png"> 
+							</div>
+							<div class="col-md-10">
+								<h1>Ecopetrol cuenta </h1>
+								<p>
+								Desde hace varios años con un Código de Ética, construido de forma participativa, el cual se constituye en el referente formal.</p>
+
+							</div>
+						</div>
+						
+					</div>
+					<div class="col-md-5" align="center" style="padding-top: 20px">
+						<img class="imagen_maseta" src="../img/iconos/maseta.png" >
+					</div>
+				</div>
+			</div>
+			<img  class="imagen_oferta" src="../img/iconos/oferta.jpg" width="250px" height="150px" >
+		</div>
+	</div>
+	<div class="col-md-12 barra_gris"></div>
+	
+<section>
+	<?php include("footer.php"); ?>
+</section>
+
+<div class="col-md-12 barra2"></div>
+
+</body>
+
+<?php include("pie.php"); ?> 
+
+</html>
